e87ac8f752e3ae04a2989ab210fe193d6d5b803c
fumiya <yuanshanzhen@gmail.com>
2026-01-23 06:56:27 +0900

    npm tsc実行とnode dist/index.js実行

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+const greeting = "hello,";
+const target = "world";
+console.log(greeting + target);
+for (let i = 0; i < 10; i++) {
+    console.log(i + "は10未満");
+}
+export {};
+//# sourceMappingURL=index.js.map
