cb20c2c557c280be23ada657030042504bd64858
fumiya <yuanshanzhen@gmail.com>
2026-01-28 08:38:23 +0900

    リテラルと真偽値と予約値について

--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,11 @@ console.log(greeting + target);
 for (let i = 0; i < 10; i++) {
     console.log(i + "は10未満");
 }
+const bigint = (123n + 456n) * 2n;
+console.log(bigint);
+console.log("hello \u20bb world");
+const yes = true;
+const no = false;
+console.log(yes, no);
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,13 @@ console.log(greeting + target)
 for(let i = 0; i < 10; i++){
 console.log(i + "は10未満")
 }
+
+const bigint :bigint = (123n + 456n) * 2n
+console.log(bigint)
+
+console.log("hello \u20bb world")
+
+const yes: boolean = true
+const no: boolean = false
+
+console.log(yes, no)
