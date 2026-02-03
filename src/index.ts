4d713a4fe11f9a7e55189c30cb26899a45ecca66
fumiya <yuanshanzhen@gmail.com>
2026-02-03 20:48:43 +0900

    2/3 力試し

--- src/index.ts
+++ src/index.ts
@@ -111,3 +111,21 @@ while(i <= 100){
     console.log(sum)
 }
 
+for(let i = 1; i < 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FIZZBUZZ")
+        continue;
+    }
+    if(i % 3 === 0){
+        console.log("FIZZ")
+        continue;
+    }
+    if(i % 5 === 0){
+        console.log("BUZZ")
+        continue;        
+    }
+    if(!(i % 3 === 0 && i % 5 === 0 )){
+        console.log(i);
+        continue;
+    }
+}
